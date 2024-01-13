f31395a32d6bb587553415c7b84486fa915b6f10
Anonlun <153767125+Anonlun@users.noreply.github.com>
2024-01-13 23:43:55 +0100

    Update MainActivity.java

--- app/src/main/java/com/luniun/luniun/time_taf/MainActivity.java
+++ app/src/main/java/com/luniun/luniun/time_taf/MainActivity.java
@@ -3,6 +3,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity
+    
 import android.widget.EditText;
 import android.widget.TextView;
 
